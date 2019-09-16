419a15c0159ffc42b0a65b115404ac200ca0acee
Dinolek <18579460+Dinolek@users.noreply.github.com>
2019-09-16 20:49:43 +0200

    Update to latest, clean up and fixes

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+
+if [ "$1" == "-h" ] || [ -z "$1" ] || [ "$1" != "wlan0" ] && [ "$1" != "ap0" ]; then
+  echo -e "Usage:
+`basename $0` ap0
+ for ap0 hotspot(mtk default)
+
+`basename $0` wlan0
+ for wlan0 hotspot(for vendor blobs usage only)"
+  exit 0
+fi
+
 rootdirectory="$PWD"
-dirs="device/mediatek/common device/mediatek/mt6735 frameworks/base kernel-3.18 vendor/mediatek/proprietary/custom vendor/mediatek/proprietary/hardware/liblights vendor/mediatek/proprietary/hardware/ril"
+dirs="device/mediatek/build device/mediatek/common device/mediatek/mt6735 frameworks/base kernel-3.18 vendor/mediatek/proprietary/custom vendor/mediatek/proprietary/hardware/liblights vendor/mediatek/proprietary/hardware/ril"
 if [ "$1" == "wlan0" ]; then
     dirs="$dirs vendor/mediatek/kernel_modules/connectivity/wlan/adaptor vendor/mediatek/kernel_modules/connectivity/wlan/core/gen2"
 fi
@@ -18,4 +29,5 @@ cp -r vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/k37tv1_64
 mv vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/mblu2/k37tv1_64_bsp.mk vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/mblu2/mblu2.mk
 sed -i 's/TARGET=k37tv1_64_bsp/TARGET=mblu2/' vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/mblu2/mblu2.mk
 cp -r patches/vendor/mediatek/proprietary/modem vendor/mediatek/proprietary/modem
+cp patches/prebuilts/misc/linux-x86/flex/flex-2.5.39 prebuilts/misc/linux-x86/flex/flex-2.5.39
 echo "Done!"
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+
 rootdirectory="$PWD"
-dirs="device/mediatek/common device/mediatek/mt6735 frameworks/base kernel-3.18 vendor/mediatek/proprietary/custom vendor/mediatek/proprietary/hardware/liblights vendor/mediatek/proprietary/hardware/ril"
-if [ "$1" == "wlan0" ]; then
+dirs="device/mediatek/build device/mediatek/common device/mediatek/mt6735 frameworks/base kernel-3.18 vendor/mediatek/proprietary/custom vendor/mediatek/proprietary/hardware/liblights vendor/mediatek/proprietary/hardware/ril"
+
+cd vendor/mediatek/kernel_modules/connectivity/wlan/adaptor
+adapter_status=$(git diff)
+cd $rootdirectory
+cd vendor/mediatek/kernel_modules/connectivity/wlan/core/gen2
+gen2_status=$(git diff)
+cd $rootdirectory
+if echo $adapter_status | grep -q wlan_if_changed && echo $gen2_status | grep -q wlan_if_changed; then
     dirs="$dirs vendor/mediatek/kernel_modules/connectivity/wlan/adaptor vendor/mediatek/kernel_modules/connectivity/wlan/core/gen2"
 fi
 
@@ -16,4 +24,7 @@ cd $rootdirectory
 rm -r device/meizu
 rm -r vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/mblu2
 rm -r vendor/mediatek/proprietary/modem
+cd prebuilts/misc
+git checkout linux-x86/flex/flex-2.5.39
+cd $rootdirectory
 echo "Done!"
